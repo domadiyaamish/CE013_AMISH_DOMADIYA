b7e0a24626d29e8e21ba4c5b99db6412d244ad8d
amish doamdiya govindbahi <70425657+domadiyaamish@users.noreply.github.com>
2022-09-19 18:20:39 +0530

    Delete lab 4/lab4_tutorial1 directory

--- lab 4/lab4_tutorial1/bin/tempCodeRunnerFile.dart
+++
@@ -1,5 +0,0 @@
- UserV4 {
-//   int id = 0;
-//   String name = "";
-//   UserV4
-// }
--- lab 4/lab4_tutorial1/test/lab4_tutorial1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:lab4_tutorial1/lab4_tutorial1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
